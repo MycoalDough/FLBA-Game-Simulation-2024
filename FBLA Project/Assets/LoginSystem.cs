860646588463ff934867c37561beddd337a6210f
MycoalDough <mycoaldough@gmail.com>
2023-11-16 00:51:31 -0800

    version 1.0.1

---
+++ FBLA Project/Assets/LoginSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LoginSystem : MonoBehaviour
+{
+
+    public string nameOfPlayer;
+    public string career;
+    public TMP_InputField field;
+    public GameObject toDiable;
+
+    public void changeCareer(string t)
+    {
+        career = t;
+    }
+
+    public void startGame()
+    {
+        if(field.text.Length > 2 && career != "")
+        {
+            field.text = nameOfPlayer;
+            toDiable.SetActive(false);
+        }
+    }
+}
---
+++ FBLA Project/Assets/Working.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Working : MonoBehaviour
+{
+
+    public TextMeshProUGUI dayCalendar;
+    public GameObject calanderDay;
+
+    public TextMeshProUGUI dailyOptionName1;
+    public TextMeshProUGUI dailyOptionName2;
+    public TextMeshProUGUI dailyOptionDesc1;
+    public TextMeshProUGUI dailyOptionDesc2;
+
+    public TextMeshProUGUI tutorial;
+
+    public TextMeshProUGUI moneyT;
+    public TextMeshProUGUI employeeT;
+    public TextMeshProUGUI knowledgeT;
+    public TextMeshProUGUI friendT;
+
+    public int money;
+    public int employee;
+    public int knowledge;
+    public int friend;
+
+    public void tutorialSet()
+    {
+        tutorial.text = "Hello, ";
+    }
+}
